cfde171adfb0535e0f117549b05fd01e91d26045
flyzero <48467541+flyzeronew@users.noreply.github.com>
2022-03-31 11:44:36 +0800

    更新

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,20 @@
-import { useState, useEffect } from "react";
-
-interface Post {
-  userId: number;
-  id: number;
-  title: string;
-  body: string;
-}
-
+import Link from 'next/link'
 export default function Home() {
-  const [post, setPost] = useState<Post>();
-
-  useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/posts/1")
-      .then((res) => res.json())
-      .then((res) => setPost(res));
-  }, []);
-
-  return (
-    <div>
-      <h1>{post?.title}</h1>
-      <p>{post?.body}</p>
-    </div>
-  );
+  <ul>
+  <li>
+    <Link href="/">
+      <a>Home1</a>
+    </Link>
+  </li>
+  <li>
+    <Link href="/about">
+      <a>About Us1</a>
+    </Link>
+  </li>
+  <li>
+    <Link href="/blog/hello-world">
+      <a>Blog Post1</a>
+    </Link>
+  </li>
+</ul>
 }
