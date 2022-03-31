d556cd00784cea1dadaa285aa08f4397f05f3bc0
flyzero <48467541+flyzeronew@users.noreply.github.com>
2022-03-31 12:03:16 +0800

    更新

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,22 @@
 import Link from 'next/link'
 export default function Home() {
-  <ul>
-  <li>
-    <Link href="/">
-      <a>Home1</a>
-    </Link>
-  </li>
-  <li>
-    <Link href="/about">
-      <a>About Us1</a>
-    </Link>
-  </li>
-  <li>
-    <Link href="/blog/hello-world">
-      <a>Blog Post1</a>
-    </Link>
-  </li>
-</ul>
+  return(
+    <ul>
+    <li>
+      <Link href="/">
+        <a>Home1</a>
+      </Link>
+    </li>
+    <li>
+      <Link href="/about">
+        <a>About Us1</a>
+      </Link>
+    </li>
+    <li>
+      <Link href="/blog/hello-world">
+        <a>Blog Post1</a>
+      </Link>
+    </li>
+  </ul>
+  )
 }
